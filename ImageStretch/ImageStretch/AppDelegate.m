c2155fb7eee3f4c0f4e03466fbf31ee3c9419b01
毛韶谦 <shao-qian.mao@utsoft.cn>
2016-12-26 12:42:08 +0800

    正式提交

--- ImageStretch/ImageStretch/AppDelegate.m
+++ ImageStretch/ImageStretch/AppDelegate.m
@@ -1,8 +1,8 @@
 //
 //  AppDelegate.m
-//  ImageStretch
+//  Text_ImageView
 //
-//  Created by 毛韶谦 on 2016/12/26.
+//  Created by 毛韶谦 on 2016/12/19.
 //  Copyright © 2016年 毛韶谦. All rights reserved.
 //
 
--- ImageStretch/ImageStretch/ViewController.m
+++ ImageStretch/ImageStretch/ViewController.m
@@ -1,8 +1,8 @@
 //
 //  ViewController.m
-//  ImageStretch
+//  Text_ImageView
 //
-//  Created by 毛韶谦 on 2016/12/26.
+//  Created by 毛韶谦 on 2016/12/19.
 //  Copyright © 2016年 毛韶谦. All rights reserved.
 //
 
@@ -10,12 +10,38 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) IBOutlet UIImageView *textImageView;
+@property (strong, nonatomic) IBOutlet UIImageView *originalImageView;
+@property (strong, nonatomic) IBOutlet UIImageView *badImageView;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    UIImage *image = [UIImage imageNamed:@"圆角矩形-7-拷贝"];
+
+    
+    //原始图片样式添加
+    self.originalImageView.image = image;
+    
+    //没处理好的图片
+    self.badImageView.image = image;
+    
+    //图片处理后的
+    [self.textImageView setImage:[image stretchableImageWithLeftCapWidth:image.size.width*0.5 topCapHeight:image.size.height*0.5]];
+    
+    
+    
+//    CGFloat top = 25; // 顶端盖高度
+//    CGFloat bottom = 25 ; // 底端盖高度
+//    CGFloat left = 10; // 左端盖宽度
+//    CGFloat right = 10; // 右端盖宽度
+//    UIEdgeInsets insets = UIEdgeInsetsMake(top, left, bottom, right);
+//    // 指定为拉伸模式，伸缩后重新赋值
+//    _textImageView.image = [_textImageView.image resizableImageWithCapInsets:insets resizingMode:UIImageResizingModeTile];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
